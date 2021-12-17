feca125f1be4ae439e4a7f31bc0257a184a02fd3
EtoMart <69122792+EtoMart@users.noreply.github.com>
2021-12-17 17:56:30 +0300

    some changes

---
+++ src/app/main/classes/form-control-value-accessor-adapter.spec.ts
@@ -0,0 +1,7 @@
+import { FormControlValueAccessorAdapter } from './form-control-value-accessor-adapter';
+
+describe('FormControlValueAccessorAdapter', () => {
+  it('should create an instance', () => {
+    expect(new FormControlValueAccessorAdapter()).toBeTruthy();
+  });
+});
